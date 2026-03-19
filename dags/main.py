d9fb40e2fc69018c8a0541dc6cc0709932de211b
Dagem Legesse <dlegess1@terpmail.umd.edu>
2026-03-19 17:06:44 -0400

    added trigger dags with dependencies. dag_produce >> dag_update >> dag_quality. Changed unit testing to reflect new trigger tasks

--- dags/main.py
+++ dags/main.py
@@ -31,14 +31,14 @@ default_args = {
 staging_schema = "staging"
 core_schema = "core"
 
-
+#DAG 1: produce_json
 with DAG(
     dag_id = 'produce_json',
     default_args=default_args,
     description='DAG to produce JSON file with raw data',
     schedule='0 14 * * *',
     catchup=False,
-) as dag:
+) as dag_produce:
 
     #Define tasks
     playlist_id = get_playlist_id()
@@ -46,36 +46,46 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
+
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db"
+
+    )
+
     #Define Dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
-
-
-
-    
+ #DAG 2: update_db   
 with DAG(
     dag_id = 'update_db',
     default_args=default_args,
     description='DAG to process JSON file and insert data into staging and core schemas',
-    schedule='0 15 * * *',
+    schedule=None, #this DAG will be triggered after produce_json finishes
     catchup=False,
-) as dag:
+) as dag_update:
 
     #Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
+
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality"
+    )
+
     #Define dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
 
-
+#DAG 3: data_quality
 with DAG(
     dag_id = 'data_quality',
     default_args=default_args,
     description='DAG to run data quality checks on both layers in db',
-    schedule='0 16 * * *',
+    schedule=None, # this dag will be run after update_db finishes
     catchup=False,
-) as dag:
+) as dag_quality:
 
     #Define tasks
     soda_validate_staging = yt_elt_data_quality(staging_schema)
